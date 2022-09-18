2828bce1ecefb208dabf1392d585a8445fe8360a
heiniux <91078374+heiniux@users.noreply.github.com>
2022-09-18 18:07:03 +0630

    hotspot.py

--- hotspotPy/hotspot.py
+++ hotspotPy/hotspot.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-#hotspot enabler
+# hotspot enabler
 import os
+
 os.system("cls")
 print("\n")
+ssid = input("Enter your hotspot ssid: ")
+key = input("Enter your hotspot password: ")
 command = ''
 while command != "exit":
+    print('\n\n\n')
     print("Type this commands : ")
     print("==> start <== for starting hotspot")
     print("==> stop  <== for stopping hotspot")
     print("==> exit  <== for exiting from programme")
     command = input("Enter yor cmd here: ")
     if command == 'start':
-        print("Starting hotspot******")
-        os.system('netsh wlan set hostednetwork mode=allow ssid=testpy key=12345678 KeyUsage=Persistent')
+        print("*****Starting hotspot******")
+        os.system('netsh wlan set hostednetwork mode=allow ssid='+ssid+' key='+key)
         os.system('netsh wlan start hostednetwork')
     elif command == 'stop':
-        print("Stopping hotspot*****")
+        print("*****Stopping hotspot*****")
         os.system('netsh wlan stop hostednetwork')
-    else:
+    elif command == 'exit':
+        print("*****Exiting from programme*****")
         exit()
-
+    else:
+        print("Wrong input please try again")
+        os.system('pause')
