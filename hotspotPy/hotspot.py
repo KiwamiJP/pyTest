56293f56b1d6068b8e72f78d3cd8179d678f4a16
heiniux <91078374+heiniux@users.noreply.github.com>
2022-09-18 17:45:40 +0630

    hotspot.py

--- hotspotPy/hotspot.py
+++ hotspotPy/hotspot.py
@@ -2,8 +2,21 @@
 #hotspot enabler
 import os
 os.system("cls")
-print("\n\n\n")
-print("\n\n\n\n\n")
-os.system('netsh wlan set hostednetwork mode=allow')
-os.system('netsh wlan set hostednetwork ssid=testpy key=12345678 KeyUsage=Persistent')
-os.system('netsh wlan start hostednetwork')
+print("\n")
+command = ''
+while command != "exit":
+    print("Type this commands : ")
+    print("start for starting hotspot")
+    print("stop for stopping hotspot")
+    print("exit for exiting from programme")
+    command = input("Enter yor cmd here: ")
+    if command == 'start':
+        print("Starting hotspot******")
+        os.system('netsh wlan set hostednetwork mode=allow ssid=testpy key=12345678 KeyUsage=Persistent')
+        os.system('netsh wlan start hostednetwork')
+    elif command == 'stop':
+        print("Stopping hotspot*****")
+        os.system('netsh wlan stop hostednetwork')
+    else:
+        exit()
+
